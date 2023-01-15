9a28e964e83c12efdc895991e9555b153001ff87
helmi0611 <110041171+helmi0611@users.noreply.github.com>
2023-01-16 00:12:03 +0700

    Add project files.

---
+++ TypingGame/Models/Stats.cs
@@ -0,0 +1,19 @@
+﻿namespace TypingGame.Models
+{
+    public class Stats
+    {
+        public int Correct;
+        public int Missed;
+        public int Total;
+        public int Accuracy;
+
+        public void total() {
+            Total = Correct + Missed;
+        }
+
+        public void CountAccuracy()
+        {
+            Accuracy = Correct * 100 / (Correct + Missed);
+        }
+    }
+}
